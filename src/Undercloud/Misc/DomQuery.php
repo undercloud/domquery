fe6c98b1424279e8c8d86e507c45568f2dc0d382
undercloud <lodashes@gmail.com>
2018-10-15 00:07:37 +0400

    Update DomQuery.php

--- src/Undercloud/Misc/DomQuery.php
+++ src/Undercloud/Misc/DomQuery.php
@@ -1,9 +1,10 @@
 <?php
 namespace Undercloud\Misc;
+
 /**
- * Memory module
+ * Easy DOM Query
  *
- * @package  Surface
+ * @package  DomQuery
  * @author   undercloud <lodashes@gmail.com>
  * @license  https://opensource.org/licenses/MIT MIT
  * @link     http://github.com/undercloud/domquery
@@ -16,8 +17,16 @@ use Exception;
 
 class DomQuery
 {
+    /**
+     * @var mixed
+     */
     private $domNodeList;
 
+    /**
+     * Constructor
+     *
+     * @param mixed $domNodeList entity
+     */
     private function __construct($domNodeList)
     {
         if (null === $domNodeList) {
@@ -27,6 +36,14 @@ class DomQuery
         $this->domNodeList = $domNodeList;
     }
 
+    /**
+     * Null value resolver
+     *
+     * @param  mixed        $key     name
+     * @param  Closure|null $closure instance
+     *
+     * @return mixed
+     */
     private function nullOr($key, Closure $closure = null)
     {
         $value = (
@@ -56,6 +73,14 @@ class DomQuery
         return $value;
     }
 
+    /**
+     * Get next DOM element
+     *
+     * @param  mixed $key   name
+     * @param  mixed $value item
+     *
+     * @return self
+     */
     private function searchNextDomElement($key, $value)
     {
         if (null === $value) {
@@ -81,16 +106,32 @@ class DomQuery
         return $value;
     }
 
+    /**
+     * Load HTML from string
+     *
+     * @param string $html value
+     *
+     * @throws Exception
+     *
+     * @return self
+     */
     public static function load($html)
     {
         $document = new DomDocument;
         if (false === @$document->loadHTML($html, LIBXML_NOBLANKS)) {
             throw new Exception('Cannot parse HTML');
         }
-    
+
         return new self([$document->documentElement]);
     }
 
+    /**
+     * Find DOM element
+     *
+     * @param string $selector target
+     *
+     * @return self
+     */
     public function find($selector)
     {
         return $this->nullOr(0, function ($node) use ($selector) {
@@ -104,21 +145,47 @@ class DomQuery
         });
     }
 
+    /**
+     * Find DOM elements by class name
+     *
+     * @param string $class selector
+     *
+     * @return self
+     */
     public function findByClass($class)
     {
     	return $this->find(".//*[contains(@class,'{$class}')]");
     }
 
+    /**
+     * Find DOM elements by id
+     *
+     * @param string $id selector
+     *
+     * @return self
+     */
     public function findById($id)
     {
     	return $this->find("//*[@id='{$id}']");
     }
 
+    /**
+     * Find DOM elements by tag name
+     *
+     * @param string $tag selector
+     *
+     * @return self
+     */
     public function findByTag($tag)
     {
     	return $this->find(".//{$tag}");
     }
 
+    /**
+     * Get element tag name
+     *
+     * @return string
+     */
     public function tagName()
     {
         return $this->nullOr(0, function ($node) {
@@ -126,6 +193,13 @@ class DomQuery
         });
     }
 
+    /**
+     * Check attribute exists
+     *
+     * @param string $name of attribute
+     *
+     * @return boolean
+     */
     public function hasAttr($name)
     {
         return $this->nullOr(0, function ($node) use ($name) {
@@ -133,6 +207,13 @@ class DomQuery
         });
     }
 
+    /**
+     * Get attribute value
+     *
+     * @param string $name of attribute
+     *
+     * @return string
+     */
     public function attr($name)
     {
         return $this->nullOr(0, function ($node) use ($name) {
@@ -140,6 +221,11 @@ class DomQuery
         });
     }
 
+    /**
+     * Convert DOM to html
+     *
+     * @return string
+     */
     public function html()
     {
         return $this->nullOr(0, function ($node) {
@@ -147,6 +233,11 @@ class DomQuery
         });
     }
 
+    /**
+     * Convert DOM to text
+     *
+     * @return string
+     */
     public function text()
     {
         return $this->nullOr(0, function ($node) {
@@ -154,11 +245,25 @@ class DomQuery
         });
     }
 
+    /**
+     * Get DOM element by index
+     *
+     * @param int $index value
+     *
+     * @return self
+     */
     public function eq($index)
     {
         return $this->nullOr($index);
     }
 
+    /**
+     * DOM collection walker
+     *
+     * @param callable $callback function
+     *
+     * @return mixed
+     */
     public function map($callback)
     {
         $map = [];
@@ -169,31 +274,61 @@ class DomQuery
         return $map;
     }
 
+    /**
+     * Get first DOM element
+     *
+     * @return self
+     */
     public function first()
     {
         return $this->nullOr('firstChild');
     }
 
+    /**
+     * Get last DOM element
+     *
+     * @return self
+     */
     public function last()
     {
         return $this->nullOr('lastChild');
     }
 
+    /**
+     *  Get next DOM element
+     *
+     * @return self
+     */
     public function next()
     {
         return $this->nullOr('nextSibling');
     }
 
+    /**
+     * Get prev DOM element
+     *
+     * @return self
+     */
     public function prev()
     {
         return $this->nullOr('previousSibling');
     }
 
+    /**
+     * Get parent node
+     *
+     * @return self
+     */
     public function parent()
     {
         return $this->nullOr('parentNode');
     }
 
+    /**
+     * Get node list count
+     *
+     * @return int
+     */
     public function length()
     {
     	if ($this->domNodeList instanceof \DOMNodeList) {
