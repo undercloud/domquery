d29b6352c6adf4c1385a3f39436c4710276e2c1e
undercloud <lodashes@gmail.com>
2020-09-25 22:53:11 +0400

    Add files via upload

--- src/Undercloud/Misc/DomQuery.php
+++ src/Undercloud/Misc/DomQuery.php
@@ -1,341 +1,47 @@
 <?php
-namespace Undercloud\Misc;
+set_time_limit(0);
+error_reporting(-1);
+ini_set('display_errors','On');
 
-/**
- * Easy DOM Query
- *
- * @package  DomQuery
- * @author   undercloud <lodashes@gmail.com>
- * @license  https://opensource.org/licenses/MIT MIT
- * @link     http://github.com/undercloud/domquery
- */
+require __DIR__ . '/lib.php';
 
-use DOMXpath;
-use DOMElement;
-use DomDocument;
-use Exception;
+$url = 'https://lightaudio.ru/album/source/?data=3d7d0a22fe-e2a176-2e276d84c77-2a30e7480';//$_GET['url'];
 
-class DomQuery
-{
-    /**
-     * @var mixed
-     */
-    private $domNodeList;
+$page = file_get_contents($url);
+$doc = new DomDocument;
+$doc->loadHTML($page);
 
-    /**
-     * Constructor
-     *
-     * @param mixed $domNodeList entity
-     */
-    private function __construct($domNodeList)
-    {
-        if (null === $domNodeList) {
-            return;
-        }
+$doc = Undercloud\Misc\DomQuery::load($doc);
 
-        $this->domNodeList = $domNodeList;
-    }
+$artist = $doc->findByClass('info')->findByTag('a')->text();
+$album  = $doc->findByClass('info')->findByTag('h1')->text();
+$logo   = $doc->findByClass('cover')->findByTag('img')->attr('src');
+$year   = $doc->findByClass('info')->findByTag('div')->findByTag('span')->eq(1)->text(); 
 
-    /**
-     * Null value resolver
-     *
-     * @param  mixed        $key     name
-     * @param  Closure|null $closure instance
-     *
-     * @return mixed
-     */
-    private function nullOr($key, Closure $closure = null)
-    {
-        $value = (
-            is_numeric($key)
-                ? $this->domNodeList[$key]
-                : (
-                    isset($this->domNodeList[0]->{$key})
-                        ? $this->domNodeList[0]->{$key}
-                        : null
-                )
-        );
+var_dump($artist,$album,$logo,$year);
 
-        $value = $this->searchNextDomElement($key, $value);
+$songs = $doc->findById('result')->findByClass('down')->map(function($node){
+	$link = 'https:' . $node->attr('href');
 
-        if (null === $value) {
-            return;
-        }
+	return (object) array(
+		'link' => $link,
+		'name' => urldecode(basename($link))
+	);
+});
 
-        if (null !== $closure) {
-            $value = $closure($value);
-        }
+var_dump(
+	$songs,
+	$doc->findById('result')->length(),
+	$doc->findById('result')->findByClass('down')->length()
+);
 
-        if ($value instanceof DOMElement) {
-            $value = new self([$value]);
-        }
+return;
+$dir = __DIR__ . '/' .$artist . '/' . $year . ' ' . $album;
+if(false == is_dir($dir)){
+//	mkdir($dir,0777,true);
+}
 
-        return $value;
-    }
-
-    /**
-     * Get next DOM element
-     *
-     * @param  mixed $key   name
-     * @param  mixed $value item
-     *
-     * @return self
-     */
-    private function searchNextDomElement($key, $value)
-    {
-        if (null === $value) {
-            return;
-        }
-
-        if ($value instanceof DOMElement) {
-            return $value;
-        }
-
-        if (in_array($key, ['firstChild', 'lastChild', 'nextSibling', 'previousSibling'], true)) {
-            if (!($value instanceof DOMElement)) {
-                if ('firstChild' === $key) {
-                    $key = 'nextSibling';
-                } elseif ('lastChild' === $key) {
-                    $key = 'previousSibling';
-                }
-            }
-
-            return $this->searchNextDomElement($key, $value->{$key});
-        }
-
-        return $value;
-    }
-
-    /**
-     * Load HTML from string
-     *
-     * @param string $html value
-     *
-     * @throws Exception
-     *
-     * @return self
-     */
-    public static function load($html)
-    {
-        $document = new DomDocument;
-        //$html = mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8');
-        if (false === @$document->loadHTML($html, LIBXML_NOBLANKS)) {
-            throw new Exception('Cannot parse HTML');
-        }
-
-        return new self([$document->documentElement]);
-    }
-
-    /**
-     * Find DOM element
-     *
-     * @param string $selector target
-     *
-     * @return self
-     */
-    public function find($selector)
-    {
-        return $this->nullOr(0, function ($node) use ($selector) {
-            return new self(
-                (new DOMXpath(
-                    $node instanceof DomDocument
-                    ? $node
-                    : $node->ownerDocument
-                ))->query($selector, $node)
-            );
-        });
-    }
-
-    /**
-     * Find DOM elements by class name
-     *
-     * @param string $class selector
-     *
-     * @return self
-     */
-    public function findByClass($class)
-    {
-    	return $this->find(".//*[contains(@class,'{$class}')]");
-    }
-
-    /**
-     * Find DOM elements by id
-     *
-     * @param string $id selector
-     *
-     * @return self
-     */
-    public function findById($id)
-    {
-    	return $this->find("//*[@id='{$id}']");
-    }
-
-    /**
-     * Find DOM elements by tag name
-     *
-     * @param string $tag selector
-     *
-     * @return self
-     */
-    public function findByTag($tag)
-    {
-    	return $this->find(".//{$tag}");
-    }
-
-    /**
-     * Get element tag name
-     *
-     * @return string
-     */
-    public function tagName()
-    {
-        return $this->nullOr(0, function ($node) {
-            return $node->tagName;
-        });
-    }
-
-    /**
-     * Check attribute exists
-     *
-     * @param string $name of attribute
-     *
-     * @return boolean
-     */
-    public function hasAttr($name)
-    {
-        return $this->nullOr(0, function ($node) use ($name) {
-            return $node->hasAttribute($name);
-        });
-    }
-
-    /**
-     * Get attribute value
-     *
-     * @param string $name of attribute
-     *
-     * @return string
-     */
-    public function attr($name)
-    {
-        return $this->nullOr(0, function ($node) use ($name) {
-            return $node->getAttribute($name);
-        });
-    }
-
-    /**
-     * Convert DOM to html
-     *
-     * @return string
-     */
-    public function html()
-    {
-        return $this->nullOr(0, function ($node) {
-            return $node->ownerDocument->saveHTML($node);
-        });
-    }
-
-    /**
-     * Convert DOM to text
-     *
-     * @return string
-     */
-    public function text()
-    {
-        return $this->nullOr(0, function ($node) {
-            return $node->textContent;
-        });
-    }
-
-    /**
-     * Get DOM element by index
-     *
-     * @param int $index value
-     *
-     * @return self
-     */
-    public function eq($index)
-    {
-        return $this->nullOr($index);
-    }
-
-    /**
-     * DOM collection walker
-     *
-     * @param callable $callback function
-     *
-     * @return mixed
-     */
-    public function map($callback)
-    {
-        $map = [];
-        foreach ($this->domNodeList as $node) {
-            $map[] = $callback(new self([$node]));
-        }
-
-        return $map;
-    }
-
-    /**
-     * Get first DOM element
-     *
-     * @return self
-     */
-    public function first()
-    {
-        return $this->nullOr('firstChild');
-    }
-
-    /**
-     * Get last DOM element
-     *
-     * @return self
-     */
-    public function last()
-    {
-        return $this->nullOr('lastChild');
-    }
-
-    /**
-     *  Get next DOM element
-     *
-     * @return self
-     */
-    public function next()
-    {
-        return $this->nullOr('nextSibling');
-    }
-
-    /**
-     * Get prev DOM element
-     *
-     * @return self
-     */
-    public function prev()
-    {
-        return $this->nullOr('previousSibling');
-    }
-
-    /**
-     * Get parent node
-     *
-     * @return self
-     */
-    public function parent()
-    {
-        return $this->nullOr('parentNode');
-    }
-
-    /**
-     * Get node list count
-     *
-     * @return int
-     */
-    public function length()
-    {
-    	if ($this->domNodeList instanceof \DOMNodeList) {
-    		return $this->domNodeList->count();
-    	}
-
-    	return count($this->domNodeList);
-    }
+//copy($logo, $dir . '/logo.jpg');
+foreach($songs as $song){
+	//copy($song->link, $dir . '/' . $song->name);
 }
